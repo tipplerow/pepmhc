8be8c5ad446258bc828b1ec36c98afffcd97d6b3
tipplerow <jsshaff@berkeley.edu>
2019-02-02 09:55:34 -0800

    Adding a global prediction method

--- src/pepmhc/engine/PredictionMethod.java
+++ src/pepmhc/engine/PredictionMethod.java
@@ -1,9 +1,32 @@
 
 package pepmhc.engine;
 
+import jam.app.JamProperties;
+
 /**
  * Enumerates peptide-MHC binding affinity prediction methods.
  */
 public enum PredictionMethod {
     NET_MHC, NET_MHC_PAN, SMM, SMM_PMBEC;
+
+    private static PredictionMethod global = null;
+
+    /**
+     * Name of the system property that specifies the global
+     * prediction method.
+     */
+    public static final String METHOD_PROPERTY = "pepmhc.engine.predictionMethod";
+
+    /**
+     * Returns the global prediction method specified through system
+     * properties.
+     *
+     * @return the global prediction method.
+     */
+    public static PredictionMethod global() {
+        if (global == null)
+            global = JamProperties.getRequiredEnum(METHOD_PROPERTY, PredictionMethod.class);
+
+        return global;
+    }
 }
