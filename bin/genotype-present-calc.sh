4fd18309e80c95e88388b73992cb491c7f61e923
tipplerow <jsshaff@berkeley.edu>
2019-02-05 11:06:02 -0800

    Renamed genotype presentation script

--- bin/genotype-present-calc.sh
+++ bin/genotype-present-calc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ########################################################################
-# Usage: pepmhc-genotype.sh [JVM OPTIONS] FILE1 [FILE2 ...]
+# Usage: genotype-present-calc.sh [JVM OPTIONS] FILE1 [FILE2 ...]
 ########################################################################
 
 if [ $# -lt 1 ]
@@ -15,13 +15,10 @@ then
     exit 1
 fi
 
-WORKDIR=`pwd`
+if [ -z "${PEPMHC_HOME}" ]
+then
+    echo "Environment variable PEPMHC_HOME is not set; exiting."
+    exit 1
+fi
 
-cd `dirname $0`
-cd ..
-
-export PEPMHC_HOME=`pwd`
-echo PEPMHC_HOME=$PEPMHC_HOME
-
-cd $WORKDIR
 ${JAM_HOME}/bin/jam-run.sh $PEPMHC_HOME pepmhc.app.GenotypePresentCalc "$@"
